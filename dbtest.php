332a11e9d751ad06657912c8524c3bfa9b04f6a6
mauromanja2018 <mauromanja2018@gmail.com>
2018-05-06 14:35:43 -0500

    d
    s

---
+++ dbtest.php
@@ -0,0 +1,16 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = 'user';
+$dbpwd = 'Mm12345$';
+$dbname = 'sindel';
+
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database");
+}
+$connection->close();
+?>
